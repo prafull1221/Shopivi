5d0a1630c524677394775e1e5ebdf609415d3bae
Prafull1221 <prafulljhariya2016@gmail.com>
2023-08-02 20:49:34 +0530

    admin side access added

--- server.js
+++ server.js
@@ -2,20 +2,29 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import authRoutes from "./routes/authRoute.js";
 import connectDB from "./config/db.js";
+import cors from "cors";
 //env
 dotenv.config();
-connectDB
+connectDB();
 //rest
 const app = express();
+
 //middelwares
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.get("/",(req,res)=>{
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+app.get("/", (req, res) => {
     res.send("<h1>welcome to Shopivi</h1>");
 });
+
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log('server running on$ ${PORT} '.bgCyan.white);
 });
