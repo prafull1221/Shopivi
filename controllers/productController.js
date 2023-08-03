4357efc3dc647e6ecdb7ade3313f739cbe480b6f
Prafull1221 <prafulljhariya2016@gmail.com>
2023-08-04 00:42:15 +0530

    create update and delete products

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,7 +7,7 @@ export const createProductController = async (req, res) => {
     const { name, description, price, category, quantity, shipping } =
       req.fields;
     const { photo } = req.files;
-    //alidation
+    //validation
     switch (true) {
       case !name:
         return res.status(500).send({ error: "Name is Required" });
