aa4ba8f193d1b6ac8d8c033c848b8f1fd0720bc9
Prafull1221 <prafulljhariya2016@gmail.com>
2023-08-04 23:10:42 +0530

    payment gateway integrated

--- client/src/components/Form/SearchInput.js
+++ client/src/components/Form/SearchInput.js
@@ -22,14 +22,14 @@ const SearchInput = () => {
     <div>
       <form className="d-flex" role="search" onSubmit={handleSubmit}>
         <input
-          className="form-control me-2"
+          className="form-control"
           type="search"
           placeholder="Search"
           aria-label="Search"
           value={values.keyword}
           onChange={(e) => setValues({ ...values, keyword: e.target.value })}
         />
-        <button className="btn btn-outline-success" type="submit">
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
           Search
         </button>
       </form>
