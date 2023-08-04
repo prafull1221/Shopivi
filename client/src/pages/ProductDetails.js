62ac2c206f66dfcd6e76052e8a2ef151db8fe28f
Prafull1221 <prafulljhariya2016@gmail.com>
2023-08-05 01:47:00 +0530

    Admin order manage and All CSS updated

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./../components/Layout/Layout";
 import axios from "axios";
+import { useCart } from "../context/cart";
+import toast from "react-hot-toast";
 import { useParams, useNavigate } from "react-router-dom";
+import "../styles/ProductDetailsStyles.css";
+
 const ProductDetails = () => {
+  const [cart, setCart] = useCart();
   const params = useParams();
   const navigate = useNavigate();
   const [product, setProduct] = useState({});
@@ -37,51 +42,93 @@ const ProductDetails = () => {
   };
   return (
     <Layout>
-      <div className="row container mt-2">
+      <div className="row container product-details">
         <div className="col-md-4">
           <img
             src={`/api/v1/product/product-photo/${product._id}`}
-            className="card-img-top-mr-2"
+            className="card-img-top"
             alt={product.name}
             width={"400px"}
             height="400px"
-            
           />
         </div>
-        <div className="col-md-6 ">
+        <div className="col-md-6 product-details-info">
           <h1 className="text-center">Product Details</h1>
-          <h6 >Name : {product.name}</h6>
+          <hr />
+          <h6>Name : {product.name}</h6>
           <h6>Description : {product.description}</h6>
-          <h6>Price : ₹{product.price}</h6>
+          <h6>
+            Price :
+            {product?.price?.toLocaleString("en-IN", {
+              style: "currency",
+              currency: "INR",
+            })}
+          </h6>
           <h6>Category : {product?.category?.name}</h6>
-          <button class="btn btn-secondary ms-1">ADD TO CART</button>
+         
+          <button
+                      className="btn btn-secondary ms-1"
+                      onClick={() => {
+                        setCart([...cart, product]);
+                        localStorage.setItem(
+                          "cart",
+                          JSON.stringify([...cart, product])
+                        );
+                        toast.success("Item Added to cart");
+                      }}
+                    >
+                      ADD TO CART
+                    </button>
         </div>
       </div>
       <hr />
-      <div className="row container">
-        <h6>Similar Products</h6>
+      <div className="row container similar-products">
+        <h4>Similar Products ➡️</h4>
         {relatedProducts.length < 1 && (
           <p className="text-center">No Similar Products found</p>
         )}
         <div className="d-flex flex-wrap">
           {relatedProducts?.map((p) => (
-            <div className="card m-2" style={{ width: "18rem" }}>
+            <div className="card m-2" key={p._id}>
               <img
-                src={`/api/v1/product/product-photo/${p?._id}`}
+                src={`/api/v1/product/product-photo/${p._id}`}
                 className="card-img-top"
                 alt={p.name}
               />
               <div className="card-body">
-                <h5 className="card-title">{p.name}</h5>
-                <p className="card-text">{p.description.substring(0, 30)}...</p>
-                <p className="card-text"> ₹ {p.price}</p>
-                <button
-                  className="btn btn-primary ms-1"
-                  onClick={() => navigate(`/product/${p.slug}`)}
+                <div className="card-name-price">
+                  <h5 className="card-title">{p.name}</h5>
+                  <h5 className="card-title card-price">
+                    {p.price.toLocaleString("en-IN", {
+                      style: "currency",
+                      currency: "INR",
+                    })}
+                  </h5>
+                </div>
+                <p className="card-text ">
+                  {p.description.substring(0, 60)}...
+                </p>
+                <div className="card-name-price">
+                  <button
+                    className="btn btn-info ms-1"
+                    onClick={() => navigate(`/product/${p.slug}`)}
+                  >
+                    More Details
+                  </button>
+                  {/* <button
+                  className="btn btn-dark ms-1"
+                  onClick={() => {
+                    setCart([...cart, p]);
+                    localStorage.setItem(
+                      "cart",
+                      JSON.stringify([...cart, p])
+                    );
+                    toast.success("Item Added to cart");
+                  }}
                 >
-                  More Details
-                </button>
-                <button class="btn btn-secondary ms-1">ADD TO CART</button>
+                  ADD TO CART
+                </button> */}
+                </div>
               </div>
             </div>
           ))}
